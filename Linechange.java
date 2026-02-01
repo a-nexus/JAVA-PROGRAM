48d4b645caff8c1e16a1ca6974cd66c245fbc257
a-nexus <anuragsingh77077@gmail.com>
2026-02-01 15:06:09 +0530

    LINE CHANGING USING BACKSLASH N

--- Linechange.java
+++ Linechange.java
@@ -1,7 +1,10 @@
 public class Linechange{
     public static void main(String[] args){
-        System.out.println("Hello World!");//By the use of ln after print its change the line
-        System.out.print("Name=Anurag Singh");
+//        System.out.println("Hello World!");//By the use of ln after print its change the line
+//        System.out.print("Name=Anurag Singh");
+//THEN WILL USE ANOTHER WAY TO CHANGE THE LINE BY USING \n
 
+        System.out.print("Hello World\n");
+        System.out.print("Name=Anurag Singh");
     }
 }
